be9588e5cb8bb1f071d8504036a2bfcd446e255e
guiloor <guille-497@hotmail.com>
2017-05-03 08:03:41 +0200

    actualizado

--- Jugador.java
+++ Jugador.java
@@ -1,67 +1,70 @@
+package ente.criaturas;
+
+	// Aqui importamos lo de josep
+	import control.Teclado; 
 
 
-// Aqui importamos lo de josep
-import control.teclado; 
-
-
-public class Jugador {
-	
-	private Teclado teclado;
-	
-	// 1º Constrcutor con esto tendremos una clase para usar para moverse
-	
-	
-	public Jugador(Teclado teclado){
-	
-	this.teclado = teclado;	
+	public class Jugador extends Criatura {
 		
-	}
-	
-	/* Asginamos un metodo para movernos
-	Aqui podremos pasarle al constructor las cordenadas en las que querramos que nuestro jugador
-	se cree en el mapa, de forma que aparezca en un lugar determinado */
-	
-	
-	
-	public Jugador(Teclado teclado, int poscionX, int posicionY)
-		this.teclado = teclado;	  // en juego/ente cambiar de privado a protected sino dara error 	
-		this.x = posicionX;
-		this.y = posicionY;
-	
-}
-	// Se encagara de actualizar datos y variables
-
-	public void actualizar() {   
+		private Teclado teclado;
 		
-		int desplazamientox = 0;
-		int desplazamientoy = 0;
-
-	//Necesitamos saber si el jugador se ha movido y donde se ha movido, aqui entra la clase teclado		
+		// 1º Constructor con esto tendremos una clase para usar para moverse
 		
-		if (teclado.arriba){
-			desplazamientoY--;
-		}
-		if (teclado.abajo){
-			desplazamientoY++;
-		}
 		
-		if (teclado.izquierda){
-			desplazamientox--;
-		}
+		public Jugador(Teclado teclado){
 		
-		if (teclado.derecha){
-			desplazamientox++;
-		}
-		
-		/* esta funcion hace que si no hemos pulsado ninguna tecla para movernos, es decir que "Y" y "X" es 0.
-		nos movremos si "x" o "y" no son "0" */
-		
-		if (desplazamientoX != 0 || desplazamientoY != 0){  
+		this.teclado = teclado;	
 			
-			mover(desplazamientoX, desplazamientoY);
-			
-	
+		}
+		
+		/* Asginamos un metodo para movernos
+		Aqui podremos pasarle al constructor las cordenadas en las que querramos que nuestro jugador
+		se cree en el mapa, de forma que aparezca en un lugar determinado */
+		
+		
+		
+		public Jugador(Teclado teclado, int posicionX, int posicionY){
+			this.teclado = teclado;	  // en juego/ente cambiar de privado a protected sino dara error 	
+			this.x = posicionX;
+			this.y = posicionY;
 		
 	}
 		
+		// Se encagara de actualizar datos y variables
+
+		public void actualizar() {   
+			
+			int desplazamientoX = 0;
+			int desplazamientoY = 0;
+
+		//Necesitamos saber si el jugador se ha movido y donde se ha movido, aqui entra la clase teclado		
+			
+			if (teclado.arriba){
+				desplazamientoY--;
+			}
+			if (teclado.abajo){
+				desplazamientoY++;
+			}
+			
+			if (teclado.izquierda){
+				desplazamientoX--;
+			}
+			
+			if (teclado.derecha){
+				desplazamientoX++;
+			}
+			
+			/* esta funcion hace que si no hemos pulsado ninguna tecla para movernos, es decir que "Y" y "X" es 0.
+			nos movremos si "x" o "y" no son "0" */
+			
+			if (desplazamientoX != 0 || desplazamientoY != 0){  
+				
+				mover(desplazamientoX, desplazamientoY);
+				
+		
+			
+		}
+			
+	}
+
 }
